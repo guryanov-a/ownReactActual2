36efc5204dce04ccc57a4815cbc4bd1c56bfb8a1
Artyom Guryanov <guryanov.artyom@gmail.com>
2023-12-14 16:51:39 +0000

    profiling: fix performance measurements on update

--- src/ownReact/reconciliation/updateComponentInstance/prepareDataForReconciliation.ts
+++ src/ownReact/reconciliation/updateComponentInstance/prepareDataForReconciliation.ts
@@ -13,7 +13,8 @@ type PrepareDataForReconciliation = (params: Params) => Result;
 export const prepareDataForReconciliation: PrepareDataForReconciliation = ({ instance, element }) => {
     instance.publicInstance.props = element.props;
 
-    const childElement = instance.publicInstance.render();
+    const childElement = instance.publicInstance.render() as Element;
+    childElement.parentElement = element;
     const oldChildInstance = instance.childInstance;
 
     return { instance: oldChildInstance, element: childElement };
